77638dfe3ec996429713925b261db1a86652e341
watashiwaaniket <aniketdhakane3@gmail.com>
2026-07-19 18:41:05 +0530

    added simple rate limiter

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,21 @@ import express from 'express';
 import { connectDB } from './lib/db.js';
 import { authRoutes } from './routes/auth.routes.js';
 import { todosRoutes } from './routes/todos.routes.js';
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    limit: 100, // limit each IP to 100 requests per windowMs
+    message: 'Too many requests, please try again later.',
+    standardHeaders: true,
+    legacyHeaders: false,
+});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(limiter);
 
 app.use('/api/auth', authRoutes);
 app.use('/api/todos', todosRoutes);
